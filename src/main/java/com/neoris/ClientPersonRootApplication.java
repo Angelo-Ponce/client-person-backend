2fec9bff526e43491d149203ac2393ebfa7daa68
Angelo Ponce <angeloponcedev@gmail.com>
2024-04-09 23:33:07 -0500

    project structure

---
+++ src/main/java/com/neoris/ClientPersonRootApplication.java
@@ -0,0 +1,13 @@
+package com.neoris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientPersonRootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientPersonRootApplication.class, args);
+    }
+
+}
